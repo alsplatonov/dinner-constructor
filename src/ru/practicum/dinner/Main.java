8c263341df6275534338adce0475139fb6797fdd
Alexander Platonov <platon1560@gmail.com>
2025-09-23 14:29:37 +0300

    add default case

--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -25,6 +25,8 @@ public class Main {
                     break;
                 case "3":
                     return;
+                default:
+                    System.out.println("Неверная команда. Пожалуйста, введите 1, 2 или 3.");
             }
         }
     }
